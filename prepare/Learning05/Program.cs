050a4aba1065db22d0ac1a903908a1789e75040f
Jordan Howard <jordanhoward1991@gmail.com>
2024-07-09 19:07:54 -0600

    adds teh Polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Blue", 4.0));
+        shapes.Add(new Rectangle("Green", 3.0, 5.0));
+        shapes.Add(new Circle("Yellow", 2.5));
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.Color}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}");
+            Console.WriteLine(); // Separate each shape's output
+        }
     }
 }
